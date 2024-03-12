0279dbe92b29c7a7781083a83d3e7af905235457
Vedant Pareek <dunefro@gmail.com>
2024-03-13 00:45:04 +0530

    Added description for output vars

--- output.tf
+++ output.tf
@@ -1,29 +1,38 @@
 ################################################################################
 # Buket
 ################################################################################
-
-output "bucket" {
-  value = var.feature_blob_storage_enabled ? module.blob_storage[0].bucket : null
-}
 output "bucket_name" {
-  value = var.feature_blob_storage_enabled ? module.blob_storage[0].name : ""
+  value       = var.feature_blob_storage_enabled ? module.blob_storage[0].name : ""
+  description = "Name of the bucket"
 }
 output "bucket_url" {
-  value = var.feature_blob_storage_enabled ? module.blob_storage[0].url : ""
+  value       = var.feature_blob_storage_enabled ? module.blob_storage[0].url : ""
+  description = "URL of the bucket"
 }
+
+################################################################################
+# Docker registry (artifact registry)
+################################################################################
+output "artifact_registry_url" {
+  value       = var.feature_docker_registry_enabled ? "${var.region}-docker.pkg.dev/${var.project}" : ""
+  description = "Artifact registry URL to connect"
+}
+
 ################################################################################
 # IAM serviceaccount
 ################################################################################
 
 output "serviceaccount_name" {
-  value = module.service_accounts.service_accounts_map
+  value       = module.service_accounts.service_accounts_map
+  description = "Name of the service account"
 }
 output "serviceaccount_keys" {
-  value     = module.service_accounts.keys
-  sensitive = true
+  value       = module.service_accounts.keys
+  sensitive   = true
+  description = "Service account keys"
 }
 
 output "serviceaccount_roles" {
-  value = local.serviceaccount_roles
-
+  value       = local.serviceaccount_roles
+  description = "Roles assigned to the TrueFoundry paltform GCP service account"
 }
