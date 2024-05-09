c7aa6d925a3a1cf484a2cca2a9f2e3a1424556af
Dipo Ajayi <ajayidipo@ymail.com>
2024-05-09 13:07:52 +0100

    removed service_account module

--- iam.tf
+++ iam.tf
@@ -1,20 +1,62 @@
-module "service_accounts" {
-  source        = "terraform-google-modules/service-accounts/google"
-  version       = "4.2.1"
-  project_id    = var.project
-  names         = [local.serviceaccount_name]
-  descriptions  = ["Truefoundry platform user with access to artifact registry, blob storage and secrets manager"]
-  display_name  = "Terraform-managed truefoundry platform service account"
-  generate_keys = true
-  project_roles = local.serviceaccount_roles
+// create service accounts
+resource "google_service_account" "service_account" {
+  account_id   = local.serviceaccount_name
+  project      = var.project
+  display_name = "Terraform-managed truefoundry platform service account"
+  description = "Truefoundry platform user with access to artifact registry, blob storage and secrets manager"
 }
 
+// custom roles for secret manager
+resource "google_project_iam_custom_role" "secret_manager_tfy_role" {
+  count       = var.feature_secrets_enabled ? 1 : 0
+
+  role_id     = replace("${local.trufoundry_platform_resources}_bucket_secret_manager_tfy_role", "-", "_")
+  title       = "Secret Manager TFY Role"
+  description = "Role to manage secrets starting with tfy"
+  permissions = [
+    "secretmanager.secrets.get",
+    "secretmanager.secrets.list",
+    "secretmanager.secrets.create",
+    "secretmanager.secrets.delete",
+    "secretmanager.secrets.update",
+    "secretmanager.versions.access",
+    "resourcemanager.projects.get",
+  ]
+}
+
+// custom role for GCS
+resource "google_project_iam_custom_role" "gcs_tfy_role" {
+  count       = var.feature_blob_storage_enabled ? 1 : 0
+
+  role_id     = replace("${local.trufoundry_platform_resources}_bucket_gcs_tfy_role", "-", "_")
+  title       = "GCS TFY Role"
+  description = "Role to manage GCS buckets starting with tfy"
+  permissions = [
+    "storage.objects.create",
+    "storage.objects.delete",
+    "storage.objects.get",
+    "storage.objects.list",
+    "storage.objects.update",
+    "storage.buckets.create",
+    "storage.buckets.get",
+    "storage.buckets.list",
+    "storage.buckets.create",
+    "storage.buckets.update",
+    "storage.multipartUploads.create",
+    "storage.multipartUploads.list",
+    "storage.multipartUploads.listParts",
+    "storage.multipartUploads.abort",
+    "resourcemanager.projects.get"
+  ]
+}
+
+// add secret manager custom roles to service account with condition
 resource "google_project_iam_member" "secret_manager_role_binding" {
   count = var.feature_secrets_enabled ? 1 : 0
 
   project = var.project
-  role    = "roles/secretmanager.admin"
-  member  = module.service_accounts.iam_email
+  role    = google_project_iam_custom_role.secret_manager_tfy_role[count.index].id
+  member  = "serviceAccount:${google_service_account.service_account.email}"
 
   condition {
     title       = "Restrict to tfy secrets"
@@ -23,16 +65,58 @@ resource "google_project_iam_member" "secret_manager_role_binding" {
   }
 }
 
+// add GCS custom roles to service account with condition
 resource "google_project_iam_member" "gcs_role_binding" {
   count = var.feature_blob_storage_enabled ? 1 : 0
 
   project = var.project
-  role    = "roles/storage.objectAdmin"
-  member  = module.service_accounts.iam_email
+  role    = google_project_iam_custom_role.gcs_tfy_role[count.index].id
+  member  = "serviceAccount:${google_service_account.service_account.email}"
 
   condition {
     title       = "Restrict to tfy storage buckets"
     description = "Allows access to buckets that start with 'tfy'"
-    expression  = "resource.name.startsWith('tfy')"
+    expression  = "resource.name.startsWith('projects/_/buckets/tfy')"
   }
+}
+
+// add service account to token creator role
+resource "google_project_iam_member" "token_creator_role_binding" {
+  count = var.feature_blob_storage_enabled ? 1 : 0
+
+  project = var.project
+  role    = "roles/iam.serviceAccountTokenCreator"
+  member  = "serviceAccount:${google_service_account.service_account.email}"
+}
+
+// add artifact registry role to service account
+resource "google_project_iam_member" "artifact_registry_role_binding" {
+  count = var.feature_docker_registry_enabled ? 1 : 0
+
+  project = var.project
+  role    = "roles/artifactregistry.admin"
+  member  = "serviceAccount:${google_service_account.service_account.email}"
+}
+
+// add container cluster viewer role to service account
+resource "google_project_iam_member" "container_cluster_viewer_role_binding" {
+  count = var.feature_cloud_integration_enabled ? 1 : 0
+
+  project = var.project
+  role    = "roles/container.clusterViewer"
+  member  = "serviceAccount:${google_service_account.service_account.email}"
+}
+
+// add container viewer role to service account
+resource "google_project_iam_member" "container_viewer_role_binding" {
+  count = var.feature_cloud_integration_enabled ? 1 : 0
+
+  project = var.project
+  role    = "roles/container.viewer"
+  member  = "serviceAccount:${google_service_account.service_account.email}"
+}
+
+// service account key
+resource "google_service_account_key" "service_account_key" {
+  service_account_id = google_service_account.service_account.id
 }
--- locals.tf
+++ locals.tf
@@ -10,11 +10,4 @@ locals {
   trufoundry_platform_resources = "${var.cluster_name}-platform"
   truefoundry_blob_storage_name = var.blob_storage_enable_override ? var.blob_storage_override_name : "${local.trufoundry_platform_resources}-bucket"
   serviceaccount_name           = trimsuffix(substr("${local.trufoundry_platform_resources}-user", 0, 30), "-")
-  serviceaccount_roles = concat(
-    var.feature_docker_registry_enabled ? ["${var.project}=>roles/artifactregistry.admin"] : [],
-    var.feature_secrets_enabled ? ["${var.project}=>roles/secretmanager.admin"] : [],
-    var.feature_blob_storage_enabled ? ["${var.project}=>roles/iam.serviceAccountTokenCreator"] : [],
-    var.feature_blob_storage_enabled ? ["${var.project}=>roles/storage.admin"] : [],
-    var.feature_cloud_integration_enabled ? ["${var.project}=>roles/container.clusterViewer", "${var.project}=>roles/container.viewer"] : []
-  )
 }
