234da773b4c78c3118f554379c84a3d1874bd51b
Harshit Luthra <love.coolsachin@gmail.com>
2024-11-21 02:43:20 +0530

    feat(output.tf): add output for IAM service account private key to enable access to service account credentials

--- output.tf
+++ output.tf
@@ -1,5 +1,14 @@
 ################################################################################
-# Buket
+# IAM serviceaccount
+################################################################################
+output "sa_auth_data" {
+  value       = base64decode(google_service_account_key.truefoundry_platform_feature_service_account_key.private_key)
+  sensitive   = true
+  description = "Private key of the service account"
+}
+
+################################################################################
+# Bucket
 ################################################################################
 output "blob_storage_enabled" {
   value       = var.feature_blob_storage_enabled
@@ -10,6 +19,7 @@ output "bucket_name" {
   value       = var.feature_blob_storage_enabled ? module.blob_storage[0].name : ""
   description = "Name of the bucket"
 }
+
 output "bucket_url" {
   value       = var.feature_blob_storage_enabled ? module.blob_storage[0].url : ""
   description = "URL of the bucket"
