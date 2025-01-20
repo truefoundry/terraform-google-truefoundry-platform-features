fdc0c1e10f611e8fc3a8e5bb8b7665934363e3dc
Dipo Ajayi <ajayidipo@ymail.com>
2025-01-20 11:10:49 +0100

    Add random characters to iam custom role

--- locals.tf
+++ locals.tf
@@ -7,7 +7,7 @@ locals {
     },
     var.tags
   )
-  trufoundry_platform_resources = "${var.cluster_name}-platform"
-  truefoundry_blob_storage_name = var.blob_storage_enable_override ? var.blob_storage_override_name : "${local.trufoundry_platform_resources}-bucket"
-  serviceaccount_name           = trimsuffix(substr("${local.trufoundry_platform_resources}-user", 0, 30), "-")
+  truefoundry_platform_resources = "${var.cluster_name}-platform-${random_string.random_id.result}"
+  truefoundry_blob_storage_name = var.blob_storage_enable_override ? var.blob_storage_override_name : "${local.truefoundry_platform_resources}-bucket"
+  serviceaccount_name           = trimsuffix(substr("${local.truefoundry_platform_resources}-user", 0, 30), "-")
 }
---
+++ utils.tf
@@ -0,0 +1,5 @@
+resource "random_string" "random_id" {
+  length  = 5
+  special = false
+  upper   = false
+}
