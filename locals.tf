ff7731d32d5a67ed1df03c9d524e7c774e49141d
Dipo Ajayi <ajayidipo@ymail.com>
2025-01-20 11:21:03 +0100

    add random provider

--- locals.tf
+++ locals.tf
@@ -8,6 +8,6 @@ locals {
     var.tags
   )
   truefoundry_platform_resources = "${var.cluster_name}-platform-${random_string.random_id.result}"
-  truefoundry_blob_storage_name = var.blob_storage_enable_override ? var.blob_storage_override_name : "${local.truefoundry_platform_resources}-bucket"
-  serviceaccount_name           = trimsuffix(substr("${local.truefoundry_platform_resources}-user", 0, 30), "-")
+  truefoundry_blob_storage_name  = var.blob_storage_enable_override ? var.blob_storage_override_name : "${local.truefoundry_platform_resources}-bucket"
+  serviceaccount_name            = trimsuffix(substr("${local.truefoundry_platform_resources}-user", 0, 30), "-")
 }
