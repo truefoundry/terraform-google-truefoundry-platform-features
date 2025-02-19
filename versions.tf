c1d198b003e6673e65a64c8e262bf8c07fb1f6ea
Harshit  Luthra <25671488+sachincool@users.noreply.github.com>
2025-02-19 20:36:13 +0530

    Merge pull request #18 from truefoundry/hl_update_version
    chore(buckets.tf): update blob_storage module version to 9.1.0 to incorporate latest features and fixes

--- versions.tf
+++ versions.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 6.11"
+      version = "~> 6.21"
     }
     random = {
       source  = "hashicorp/random"
