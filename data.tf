2fc41589f6ccb26d11aedcc861690d7260844523
Vedant Pareek <dunefro@gmail.com>
2026-06-05 21:10:42 +0530

    Adding support of WIF for exisitng serviceaccount

--- data.tf
+++ data.tf
@@ -1,3 +1,8 @@
 data "google_project" "truefoundry_platform_feature_project" {
   project_id = var.project
 }
+
+data "google_service_account" "truefoundry_platform_feature_service_account" {
+  count      = var.service_account_enabled ? 0 : 1
+  account_id = var.existing_service_account_email
+}
--- iam.tf
+++ iam.tf
@@ -187,8 +187,7 @@ resource "google_project_iam_member" "truefoundry_platform_feature_additional_ro
 }
 
 resource "google_service_account_iam_binding" "truefoundry_platform_feature_service_account_workload_identity_binding" {
-  count              = var.service_account_enabled ? 1 : 0
-  service_account_id = google_service_account.truefoundry_platform_feature_service_account[0].id
+  service_account_id = var.service_account_enabled ? google_service_account.truefoundry_platform_feature_service_account[0].id : data.google_service_account.truefoundry_platform_feature_service_account[0].id
   role               = "roles/iam.workloadIdentityUser"
 
   members = compact([
@@ -198,7 +197,7 @@ resource "google_service_account_iam_binding" "truefoundry_platform_feature_serv
 
 // service account key
 resource "google_service_account_key" "truefoundry_platform_feature_service_account_key" {
-  count              = var.service_account_enabled && var.service_account_key_creation_enabled ? 1 : 0
+  count              = var.service_account_key_creation_enabled ? 1 : 0
   service_account_id = google_service_account.truefoundry_platform_feature_service_account[0].id
 }
 
@@ -208,7 +207,7 @@ resource "google_service_account_key" "truefoundry_platform_feature_service_acco
 
 // workload identity pool
 resource "google_iam_workload_identity_pool" "truefoundry_platform_feature_pool" {
-  count                     = var.service_account_enabled && var.service_account_keyless_enabled ? 1 : 0
+  count                     = var.service_account_keyless_enabled ? 1 : 0
   project                   = var.project
   workload_identity_pool_id = substr("${var.cluster_name}-tfy-pool", 0, 32)
   display_name              = "${var.cluster_name}-tfy-pool"
@@ -217,7 +216,7 @@ resource "google_iam_workload_identity_pool" "truefoundry_platform_feature_pool"
 
 // OIDC provider for the workload identity pool
 resource "google_iam_workload_identity_pool_provider" "truefoundry_platform_feature_oidc_provider" {
-  count                              = var.service_account_enabled && var.service_account_keyless_enabled ? 1 : 0
+  count                              = var.service_account_keyless_enabled ? 1 : 0
   project                            = var.project
   workload_identity_pool_id          = google_iam_workload_identity_pool.truefoundry_platform_feature_pool[0].workload_identity_pool_id
   workload_identity_pool_provider_id = substr("${var.cluster_name}-tfy-oidc", 0, 32)
@@ -245,4 +244,9 @@ moved {
 moved {
   from = google_service_account_iam_binding.truefoundry_platform_feature_flyte_propeller_service_account_binding[0]
   to   = google_service_account_iam_binding.truefoundry_platform_feature_service_account_workload_identity_binding[0]
+}
+
+moved {
+  from = google_service_account_iam_binding.truefoundry_platform_feature_service_account_key_binding[0]
+  to   = google_service_account_iam_binding.truefoundry_platform_feature_service_account_key_binding
 }
